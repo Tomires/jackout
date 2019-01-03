5d94006bb0bb5580fb6106cbe1effcc0222dbec2
Bc. Tomáš Havlík <havlito5@fel.cvut.cz>
2019-01-03 22:01:04 +0100

    Added light switch

---
+++ Assets/Scripts/Interaction/LightSwitchButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jackout.Interaction {
+	public class LightSwitchButton : BaseButton {
+		public Logic.LightControl[] lights;
+		public bool lightsOn = false;
+		public SpriteRenderer spriteRenderer;
+		public Sprite spriteOn, spriteOff;
+
+		public override void Action() {
+			lightsOn = !lightsOn;
+			spriteRenderer.sprite = lightsOn ? spriteOn : spriteOff;
+
+			foreach(Logic.LightControl light in lights) {
+				light.Switch(lightsOn);
+			}
+		}
+	}
+}

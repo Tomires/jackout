8fb4d8ac00d251d0f1c4acfed1fbd40c5d0fc511
Bc. Tomáš Havlík <havlito5@fel.cvut.cz>
2019-01-08 11:04:29 +0100

    Fix grabbing behaviour for certain objects

---
+++ Assets/Scripts/Interaction/HeadsetGrab.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jackout.Interaction {
+    public class HeadsetGrab : BaseGrab
+    {
+        public override void PickedUp() {
+            
+        }
+
+        public override void Dropped() {
+            
+        }
+    }
+}
